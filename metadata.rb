ae6a2921e7116ea1741662bef99ce35f981cec92
James Hu <hello@james.hu>
2014-01-30 15:00:38 -0800

    Fix variable typo

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "hello@james.hu"
 license          "Apache 2.0"
 description      "Provides recipes for installing wp-cli."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 recipe "default", "Installs wp-cli"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,7 @@
 
 executable_path = "#{node['wp-cli']['path']}/#{node['wp-cli']['executable']}"
 
-remote_file path do
+remote_file executable_path do
   source "https://github.com/wp-cli/wp-cli/releases/download/v#{node['wp-cli']['version']}/wp-cli.phar"
   not_if "test -f #{executable_path}"
   notifies :run, "execute[make-binary-executable]", :immediately
