925b3b80376c276b6fdd4b6cb670b675a96b1ead
James Hu <hello@james.hu>
2016-01-11 18:48:54 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "hello@james.hu"
 license          "Apache 2.0"
 description      "Provides recipes for installing wp-cli."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 recipe "default", "Installs wp-cli"
 
